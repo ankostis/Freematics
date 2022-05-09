e0da511cd117b7d0f0366941f651a47c7eab245e
Kostis Anagnostopoulos <ankostis@gmail.com>
2022-05-10 00:17:58 +0300

    refact(fw5) `state.py` retrofitted to return states >
    (not already formatted messages), anticipating its use as a library
    for the  pio-monitor filter.
    
    + doc: sample code calling it; +file docstring.

--- firmware_v5/telelogger/freestate.py
+++ firmware_v5/telelogger/freestate.py
@@ -1,36 +1,50 @@
 #!/usr/bin/env python
-#
-# SYNTAX: ./freestate.py <hex-number>
-# Decode firmware hex-code states
+"""
+Decode firmware hex-code states
+
+SYNTAX:
+    ./freestate.py <hex-number>
+
+EXAMPLE:
+    ./freestate.py 0x23c
+    STATE:  0x23c --> 0000_0010_0011_1100
+    0(   0x4): GPS_READY
+    1(   0x8): MEMS_READY
+    2(  0x10): NET_READY
+    3(  0x20): NET_CONNECTED
+    4( 0x200): GET_OBFCM
+"""
 
 import sys
 
 flags = {
-   0x1: "STATE_STORAGE_READY",
-   0x2: "STATE_OBD_READY",
-   0x4: "STATE_GPS_READY",
-   0x8: "STATE_MEMS_READY",
-   0x10: "STATE_NET_READY",
-   0x20: "STATE_NET_CONNECTED",
-   0x40: "STATE_WORKING",
-   0x100: "STATE_STANDBY",
-   0x200: "STATE_GET_OBFCM",
-   0x800: "STATE_SEND_ITID17",
-   0x1000: "STATE_SEND_ITID1A",
-   0x2000: "STATE_SEND_ITID1B",
-   0x4000: "STATE_SEND_ITID1C",
+   0x1: "STORAGE_READY",
+   0x2: "OBD_READY",
+   0x4: "GPS_READY",
+   0x8: "MEMS_READY",
+   0x10: "NET_READY",
+   0x20: "NET_CONNECTED",
+   0x40: "WORKING",
+   0x100: "STANDBY",
+   0x200: "GET_OBFCM",
+   0x800: "SEND_ITID17",
+   0x1000: "SEND_ITID1A",
+   0x2000: "SEND_ITID1B",
+   0x4000: "SEND_ITID1C",
 }
-def status_names(status: int):
+def state_flag_n_names(state: int):
     for i in range(16):
         flag = (1 << i)
-        if status & flag:
-            yield f"{i}({flag:#6x}): {flags.get(flag, '-')}"
+        if state & flag:
+            yield flag, flags.get(flag, '-')
 
 
-def main(hex_status: str):
-    status = int(hex_status, 16)
-    print(f"STATUS: {status:#6x} --> {status:019_b}")
-    print("\n".join(status_names(status)))
+def main(hex_state: str):
+    state = int(hex_state, 16)
+
+    print(f"STATE: {state:#6x} --> {state:019_b}")
+    for i, (flag, name) in enumerate(state_flag_n_names(state)):
+        print(f"{i}({flag:#6x}): {name}")
 
 if __name__ == "__main__":
     main(sys.argv[1])
