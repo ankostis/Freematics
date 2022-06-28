ankostis/Freematics

commit e0acf760eda621d399ca6bede250b644184ad0ec

WIP/enh(STDBY) new activation trigger voltage

firmware_v5/telelogger/telelogger.ino
@@ -1112,11 +1112,32 @@ void standby()
   Serial.println("STANDBY");
   obd.enterLowPowerMode();
 #if ENABLE_MEMS
+  uint32_t t;
+  float v;
+  uint32_t t_old = millis();
+  float v_old = 999;
+  float v_grad;
+
   calibrateMEMS();
-  waitMotion(-1);
+
+  do {
+    t = millis();
+    v = (float)obd.getVoltage();
+    v_grad = (v - v_old)/(t - t_old)*1000;
+    Serial.print("Voltage old: ");
+    Serial.print(v_old);
+    Serial.print(", Voltage new: ");
+    Serial.print(v);
+    Serial.print(", Gradient: ");
+    Serial.println(v_grad);
+    delay(1000);
+    t_old = t;
+    v_old = v;
+  } while ((!((v > THR_VOLTAGE) && (v_grad > THR_GRAD))) || (v_grad > 4));
+  // waitMotion(-1);
 #elif ENABLE_OBD
   do {
-    delay(5000);
+    delay(1000);
   } while (obd.getVoltage() < JUMPSTART_VOLTAGE);
 #else
   delay(5000);

commit 073768d4c917e50e4fde740044cb3e7c2a2b6982

fix(fw5) unguarded `obd`access when ENABLE_OBD=1 >
regression introduced by JRC with e0acf76 on 21 Apr 2021
(to be expected on that day ;-).

firmware_v5/telelogger/telelogger.ino
@@ -1285,7 +1285,11 @@ void standby()
   do {
       if (ledMode == 0) digitalWrite(PIN_LED, HIGH);
     t = millis();
+#if ENABLE_OBD
     v = obd.getVoltage();
+#else  // ENABLE_OBD
+    v = THR_VOLTAGE;
+#endif  // ENABLE_OBD
 
     v_grad = (v - v_old)/(t - t_old)*1000;
     ESP_LOGI(