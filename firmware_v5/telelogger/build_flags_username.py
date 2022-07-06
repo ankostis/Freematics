8781ed51ba78e526a91815fc4f3f9bbf6b018894
Kostis Anagnostopoulos <ankostis@gmail.com>
2022-07-07 00:13:26 +0300

    DROP(fw5.BUILD) ota-patching replaced ad-hoc pythons

--- firmware_v5/telelogger/build_flags_username.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-#
-# Produce a dynamic *build_flags* for `platformio.in`.
-
-import getpass, socket
-
-print(f"-DBUILD_USERNAME='\"{getpass.getuser()}\"'")
-print(f"-DBUILD_HOSTNAME='\"{socket.gethostname()}\"'")
